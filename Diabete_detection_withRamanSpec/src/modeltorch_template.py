356ac71e0680a6965a3a83929132f2d6b6cacccb
Ati chets <ksde34@gmail.com>
2022-11-30 09:31:04 +0700

    add dropout a lot

--- Diabete_detection_withRamanSpec/src/modeltorch_template.py
+++ Diabete_detection_withRamanSpec/src/modeltorch_template.py
@@ -127,3 +127,42 @@ class RamConv1d_bnmx(nn.Module):
         x = self.fc2(x)
         
         return x
+
+
+
+class RamConv1d_mx(nn.Module):
+    def __init__(self, input_size = 1, hidden_size=10,hidden_size2=50,linear_hidden=14, out_size=2):
+        super().__init__()
+        self.conv1 = nn.Conv1d(input_size, hidden_size, kernel_size = 3)      
+        self.relu1 = nn.ReLU()                            
+        # Convolution Layer 2
+        self.conv2 = nn.Conv1d(hidden_size, hidden_size2, kernel_size = 3)     
+        self.maxpool1 = nn.MaxPool1d(2)
+        self.maxpool2 = nn.MaxPool1d(2)                   
+        self.relu2 = nn.ReLU()                           
+        self.drop = nn.Dropout(p=0.5)
+
+        self.fc1 = nn.Linear(12400, 1000)
+        self.fc2 = nn.Linear(1000, linear_hidden)
+        self.fc3 = nn.Linear(linear_hidden, out_size)
+
+    def forward(self, x):
+        
+        # Convolution Layer 1                    
+        x = self.conv1(x)                                                    
+        x = self.relu1(x)
+        x = self.maxpool1(x) 
+        # Convolution Layer 2
+        x = self.conv2(x)  
+        x = self.relu2(x)             
+        x = self.maxpool2(x)
+
+        x = x.reshape(x.size(0), -1)
+
+        x = self.fc1(x)
+        x = self.relu1(x)
+
+        x = self.drop(x)
+        x = self.fc2(x)
+        x = self.relu1(x)
+        return self.fc3(x)
