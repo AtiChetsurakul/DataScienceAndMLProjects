fe3936ff2d214e7a823d07daa767233c94ab1510
Ati (chets) tesakulsiri <ksde34@gmail.com>
2022-12-01 11:57:10 +0700

    update starter

--- Diabete_detection_withRamanSpec/starter.sh
+++ Diabete_detection_withRamanSpec/starter.sh
@@ -1,2 +1,6 @@
 # pip install BaselineRemoval;
+mkdir dataset;
+# wget https://www.kaggle.com/datasets/codina/raman-spectroscopy-of-diabetes/download?datasetVersionNumber=8;
+# mv archive.zip dataset/;
+# unzip dataset/archive.zip;
 pip install -r requirements.txt;
